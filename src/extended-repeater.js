55ef37b799262fb01af2d0add2fd1608f6810cf1
MarkDoroshenko <msv0804@gmail.com>
2023-12-19 17:31:17 +0200

    init: proceed js-basic task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,52 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require( '../extensions/index.js' );
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
+ *
  * repeater('STRING', { repeatTimes: 3, separator: '**', 
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater( str, options ) {
+    const strIn = typeof str === 'string' ? str : String( str );
+    let separator = options.separator ? options.separator : '+';
+    let addSeparator = options.additionSeparator ? options.additionSeparator : '|';
+    let repeatTimes = options.repeatTimes ? options.repeatTimes : 1;
+    let addRepeatTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+    let addition = '';
+    if ( options.hasOwnProperty('addition') ) {
+        for ( let i = 0; i < addRepeatTimes; i++ ) {
+            if ( i === addRepeatTimes - 1 ) {
+                addition += options.addition
+            } else {
+                addition += `${ options.addition }${ addSeparator }`;
+            }
+        }
+    }
+    let res = '';
+    for ( let i = 0; i < repeatTimes; i++ ) {
+        debugger
+        if ( i === repeatTimes - 1 ) {
+            res += addition ? `${ strIn }${ addition }` : strIn;
+        } else {
+            const val = addition ? `${ strIn }${ addition }${ separator }` : `${ strIn }${ separator }`;
+            console.log( '`${ strIn }${ separator }`', `${ strIn }${ separator }` );
+            console.log( 'val', val );
+            res += val;
+        }
+    }
+    return res;
 }
 
+console.log( 'repeater', repeater( 'la', { repeatTimes: 3 } ) )
 module.exports = {
-  repeater
+    repeater
 };
--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require( '../extensions/index.js' );
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+    const nameCounts = {};
+    const result = [];
+
+    function getUniqueName(name, count) {
+        const newName = count === 0 ? name : `${name}(${count})`;
+
+        if (nameCounts[newName] !== undefined) {
+            return getUniqueName(name, count + 1);
+        }
+
+        nameCounts[newName] = 1;
+        return newName;
+    }
+
+    names.forEach((name) => {
+        result.push( getUniqueName(name, 0));
+    });
+
+    return result;
 }
 
+console.log( 'renameFiles', renameFiles( [ "file", "file", "image", "file(1)", "file" ] ) )
 module.exports = {
-  renameFiles
+    renameFiles
 };
+
+// } else if ( resArr.includes( setObj[el] ) ) {
+//     console.log( 'setObj[el]', setObj[el] )
+//     resArr.push( `${ el }(${ setObj[el] })` )
